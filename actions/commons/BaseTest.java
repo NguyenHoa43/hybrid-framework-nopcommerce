a446f305c10b233cf101e6c6fe8dd6c78823db15
Nguyen Hoa <nguyendhoa1243@gmail.com>
2023-06-13 20:43:47 +0700

    update Repository

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,5 +1,31 @@
 package commons;
 
-public class BaseTest {
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BaseTest {
+	private String projectPath = System.getProperty("user.dir");
+	private WebDriver driverBaseTest;;
+	protected WebDriver getBrowserDriver(String browserName ) {
+		if(browserName.equals("firefox")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+			driverBaseTest = new FirefoxDriver();
+		}else if(browserName.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+			driverBaseTest = new ChromeDriver();
+		}else if(browserName.equals("edge")) {
+			System.setProperty("webdriver.edge.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
+			driverBaseTest = new EdgeDriver();
+		}else {
+			throw new RuntimeException("Browser name invalid !");
+		}
+		driverBaseTest.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driverBaseTest.get("https://demo.nopcommerce.com/");
+		return driverBaseTest;
+	}
+	
 }
--- testcase/com/nopcommerce/user/lever_04_Multiple_Browser.java
+++ testcase/com/nopcommerce/user/lever_04_Multiple_Browser.java
@@ -8,27 +8,24 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import commons.BaseTest;
 import pageObject.HomePageObject;
 import pageObject.LoginPageObject;
 import pageObject.RegisterPageObject;
 
-public class lever_03_Page_Object_02_Login {
-	private WebDriver driver;
-	private String projectPath = System.getProperty("user.dir");
+public class lever_04_Multiple_Browser extends BaseTest {
+	private WebDriver driverTestClass;
 	private String  firstName, lastName, passWord, emailAddress, wrongPassword, wrongEmail;;
 	private LoginPageObject loginPage;
 	private RegisterPageObject registerPage;
-	private HomePageObject homePage;
-	
+	private HomePageObject homePage; 
+	@Parameters("browser")
 	@BeforeClass
-	  public void beforeClass() {
-		  System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
-		  driver = new FirefoxDriver();
-		  driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		  driver.get("https://demo.nopcommerce.com/");
-		  
+	  public void beforeClass(String browserName) {
+		  driverTestClass = getBrowserDriver(browserName);
 		  emailAddress = "afc" + fadeNumber() + "@gmail.com";
 		  firstName = "Automation";
 		  lastName = "FC";
@@ -36,9 +33,9 @@ public class lever_03_Page_Object_02_Login {
 		  wrongPassword = "123";
 		  wrongEmail = "123@456#%*"; 
 		  
-		  homePage = new HomePageObject(driver);
-		  registerPage = new RegisterPageObject(driver);
-		  loginPage = new LoginPageObject(driver);
+		  homePage = new HomePageObject(driverTestClass);
+		  registerPage = new RegisterPageObject(driverTestClass);
+		  loginPage = new LoginPageObject(driverTestClass);
 		  
 		  homePage.clickToRegisterLink();
 		  registerPage.inputToFirstnameTextbox(firstName);
@@ -49,15 +46,12 @@ public class lever_03_Page_Object_02_Login {
 		  registerPage.clickToRegisterButton();
 		  Assert.assertEquals(registerPage.getRegisterSuccessMessage(),"Your registration completed");
 	}
-	
 	@Test
 	public void Login_01_Empty_Data() {
 		registerPage.clickToLoginLink();
 		loginPage.clickToLoginButton();
-		Assert.assertEquals(loginPage.getErrorMessageAtEmailTextbox(), "Please enter your email");
-		
-	}
-	
+		Assert.assertEquals(loginPage.getErrorMessageAtEmailTextbox(), "Please enter your email");		
+	}	
 	@Test
 	public void Login_02_Invalid_Email() {
 		registerPage.clickToLoginLink();
@@ -65,8 +59,7 @@ public class lever_03_Page_Object_02_Login {
 		loginPage.inputToPasswordTextbox(passWord);
 		loginPage.clickToLoginButton();
 		Assert.assertEquals(loginPage.getErrorMessageAtEmailTextbox(), "Wrong email");
-	}
-	
+	}	
 	@Test
 	public void Login_03_Unregister_Email() {
 		registerPage.clickToLoginLink();
@@ -75,15 +68,13 @@ public class lever_03_Page_Object_02_Login {
 		loginPage.clickToLoginButton();
 		Assert.assertEquals(loginPage.getErrorMessageEmailUnregister(), "Login was unsuccessful. Please correct the errors and try again.\nNo customer account found");
 	}
-	
 	@Test
 	public void Login_04_Empty_Password() {
 		registerPage.clickToLoginLink();
 		loginPage.inputToEmailTextbox(emailAddress);
 		loginPage.clickToLoginButton();
 		Assert.assertEquals(loginPage.getErrorMessageAtPasswordTextbox(), "Login was unsuccessful. Please correct the errors and try again.\nThe credentials provided are incorrect");
-	}
-	
+	}	
 	@Test
 	public void Login_05_Wrong_Password() {
 		registerPage.clickToLoginLink();
@@ -92,7 +83,6 @@ public class lever_03_Page_Object_02_Login {
 		loginPage.clickToLoginButton();
 		Assert.assertEquals(loginPage.getErrorMessageAtWrongPasswordTextbox(),"Login was unsuccessful. Please correct the errors and try again.\nThe credentials provided are incorrect");
 	}
-	
 	@Test
 	public void Login_06_Successfull() {
 		registerPage.clickToLoginLink();
@@ -100,14 +90,13 @@ public class lever_03_Page_Object_02_Login {
 		loginPage.inputToPasswordTextbox(passWord);
 		loginPage.clickToLoginButton();
 		Assert.assertTrue(homePage.isMyAccountLinkDisplay());
-	}
-	
+	}	
 	public int fadeNumber() {
 		Random rand = new Random();
 		return rand.nextInt(9999);
 	}
 	@AfterClass
 	public void afterClass() {
-		driver.quit();
+		driverTestClass.quit();
 	}
 }
