9ef7316090925cc4c74ef6b3c46bfcd8414443b5
Nguyen Hoa <nguyendhoa1243@gmail.com>
2023-06-14 14:47:16 +0700

    Multiple Browser Auto Update Driver (Chrome - Edge - Opera - Firefox - HeadlessChrome - HeadlessFirefox - Coccoc)

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -4,26 +4,52 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.opera.OperaDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
-	private String projectPath = System.getProperty("user.dir");
+	
 	private WebDriver driverBaseTest;;
 	protected WebDriver getBrowserDriver(String browserName ) {
 		if(browserName.equals("firefox")) {
-			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+			WebDriverManager.firefoxdriver().setup();
 			driverBaseTest = new FirefoxDriver();
+		}else if(browserName.equals("headlessfirefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			FirefoxOptions options = new FirefoxOptions();
+			options.addArguments("--headless");
+			options.addArguments("window-size=1920x1080");
+			driverBaseTest = new FirefoxDriver(options);
+		}else if(browserName.equals("coccoc")) {
+			WebDriverManager.chromedriver().driverVersion("112.0.5615.28").setup();
+			ChromeOptions options = new ChromeOptions();
+			options.setBinary("C:\\Program Files\\CocCoc\\Browser\\Application\\browser.exe");
+			driverBaseTest = new ChromeDriver(options);
 		}else if(browserName.equals("chrome")) {
-			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+			WebDriverManager.chromedriver().setup();
 			driverBaseTest = new ChromeDriver();
-		}else if(browserName.equals("edge")) {
-			System.setProperty("webdriver.edge.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
+		}else if(browserName.equals("headlesschrome")) {
+			WebDriverManager.chromedriver().setup();
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--headless");
+			options.addArguments("window-size=1920x1080");
+			driverBaseTest = new ChromeDriver(options);
+		}else if(browserName.equals("opera")) {
+			WebDriverManager.operadriver().setup();
+			driverBaseTest = WebDriverManager.operadriver().create();
+		}
+		else if(browserName.equals("edge")) {
+			WebDriverManager.edgedriver().setup();
 			driverBaseTest = new EdgeDriver();
 		}else {
 			throw new RuntimeException("Browser name invalid !");
 		}
-		driverBaseTest.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driverBaseTest.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
 		driverBaseTest.get("https://demo.nopcommerce.com/");
 		return driverBaseTest;
 	}
